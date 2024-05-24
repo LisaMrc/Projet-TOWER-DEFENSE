ac70bf897a53d86526a1457ce85c09f82188352d
Justine <justineperso03@gmail.com>
2024-05-24 08:52:07 +0200

    debug premieres struc

--- src/code/entities/entities.hpp
+++ src/code/entities/entities.hpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #pragma once
 
-struct knight {
+struct knight
+{
     int health {200};
     int speed {8};
-    // int dammage {50};
+    // int damage {50};
     int gold {30};
 };
 
-struct wizzard {
+struct wizard {
     int health {80};
     int speed {4};
     // int dammage {100};
@@ -19,6 +20,6 @@ struct tower {
     int power {20};
     int range {10};
     int rate {1}; // tir par seconde
-}
+};
 
-int gold_earned{0};
+extern int gold_earned; //extern pour mettre juste la "methode" de la variable (commme les fonctions)
